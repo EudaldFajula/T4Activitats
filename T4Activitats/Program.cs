49cb5095f7d440b677492905e4c4e9f986d51637
Eudald Fajula <eudald.fajula.7e6@itb.cat>
2025-02-03 15:58:34 +0100

    Act3 finish

--- T4Activitats/Program.cs
+++ T4Activitats/Program.cs
@@ -13,17 +13,30 @@ namespace Activitats
             //Parella<double, bool> parellaTest2 = new Parella<double, bool>(3.4, true);
             //parellaTest2.MostrarValors();
             //Act2
-            ArrayList arrayList = new ArrayList();
+            //ArrayList arrayList = new ArrayList();
+            //Console.WriteLine("Cuants valors vols?");
+            //int longitud = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 0; i < longitud; i++)
+            //{
+            //    Console.WriteLine("Escriu valor: ");
+            //    arrayList.Add(Console.ReadLine());
+            //}
+            //foreach (var i in arrayList)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Act 3
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
             Console.WriteLine("Cuants valors vols?");
             int longitud = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < longitud; i++)
+            for (int i = 1; i < longitud + 1; i++)
             {
-                Console.WriteLine("Escriu valor: ");
-                arrayList.Add(Console.ReadLine());
+                Console.WriteLine("Escriu quines edats vols introduir?");
+                dictionary.Add($"Alumne {i}:", Convert.ToInt32(Console.ReadLine()));
             }
-            foreach (var i in arrayList)
+            foreach (var items in dictionary)
             {
-                Console.WriteLine(i);
+                Console.WriteLine($"{items.Key} {items.Value}");
             }
         }
     }
