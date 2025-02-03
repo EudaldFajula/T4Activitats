ebfa5d196a230a22a8b690089a0fb949c164d0dd
Eudald Fajula <eudald.fajula.7e6@itb.cat>
2025-02-03 12:31:08 +0100

    Acabat Act1

--- T4Activitats/Program.cs
+++ T4Activitats/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using T4Activitats;
+namespace Activitats
+{
+    public class Act
+    {
+        public static void Main()
+        {
+            Parella<string, int> parellaTest1 = new Parella<string, int>("Hola", 2);
+            parellaTest1.MostrarValors();
+            Parella<double, bool> parellaTest2 = new Parella<double, bool>(3.4, true);
+            parellaTest2.MostrarValors();
+        }
+    }
+}
